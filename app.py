87836c1146756bd72655d478ddbe1689dd29d3f7
CantarzoTDC <25806400+CantarzoTDC@users.noreply.github.com>
2021-10-30 14:44:03 -0300

    progress? I don't think so

--- app.py
+++ app.py
@@ -11,7 +11,21 @@ def index():  # put application's code here
     return render_template('index.html')
 
 
-@app.route('/pesquisa', methods=['get'])
+#
+# @app.route('/pesquisa', methods=['GET', 'POST'])
+# def search():
+#     form = SearchForm()
+#     if request.method == 'POST' and form.validate_on_submit():
+#         q = request.form.get('q')
+#         if (q == ''):
+#             url = 'https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&order=cmc&page=1&q=c%3C=wbgru&unique=cards&pretty=true'
+#         else:
+#             url = 'https://api.scryfall.com/catalog/card/search?q=' + q + '&format=json&pretty=true'
+#         dados = requests.get(url)
+#     return render_template('pesquisa.html', form=form)
+
+
+@app.route('/pesquisa', methods=['get', 'post'])
 def getCard():
     q = request.form.get('q')
     if (q == ''):
@@ -19,23 +33,11 @@ def getCard():
     else:
         url = 'https://api.scryfall.com/catalog/card/search?q=' + q + '&format=json&pretty=true'
     dados = requests.get(url)
-    nome = None
-    imguri = None
-    custo_m = None
-    cmc = None
-    p_r = None
-    cor = None
-    refs = None
 
     #    if dados
 
     nome: str = dados['data'][0]['name']
     imguri = dados['data'][0]['image_uris']['normal']
-    idioma: str = dados['data'][0]['lang']
-    custo_m: str = dados['data'][0]['mana_cost']
-    cmc: str = dados['data'][0]['cmc']
-    p_r: str = (dados['data'][0]['power'] + '/' + dados['data'][0]['toughness'])
-    cor: str = dados['data'][0]['colors']
     refs = dados['data'][0]['related_uris']['gatherer']
     more = dados['has_more']
     if more == True:
@@ -43,7 +45,7 @@ def getCard():
     else:
         nxt = ''
 
-    return render_template('pesquisa.html')
+    return render_template('pesquisa.html', form=form)
 
 
 @app.route('/NAMEme', methods=['get', 'post'])
