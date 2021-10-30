799aa84e599f0c825d5b65d3101e7fe6503d529f
CantarzoTDC <25806400+CantarzoTDC@users.noreply.github.com>
2021-10-29 22:37:00 -0300

    progress? I don't think so

--- app.py
+++ app.py
@@ -13,20 +13,19 @@ def index():  # put application's code here
 
 @app.route('/pesquisa', methods=['get'])
 def getCard():
-    dados:json
     q = request.form.get('q')
-    if q == '':
+    if (q == ''):
         dados = requests.get('https://api.scryfall.com/cards/search?format=json&include_extras=false&include_multilingual=false&order=cmc&page=1&q=c%3C=wbgru&unique=cards&pretty=true').json()
     else:
         dados = requests.get('https://api.scryfall.com/catalog/card/search?q='+q+'&format=json&pretty=true').json()
 
-    nome = dados['data'][0]['name']
+    nome:str = dados['data'][0]['name']
     imguri = dados['data'][0]['image_uris']['normal']
-    idioma = dados['data'][0]['lang']
-    custo_m = dados['data'][0]['mana_cost']
-    cmc = dados['data'][0]['cmc']
-    p_r = (dados['data'][0]['power'] + '/' + dados['data'][0]['toughness'])
-    cor = dados['data'][0]['colors']
+    idioma:str = dados['data'][0]['lang']
+    custo_m:str = dados['data'][0]['mana_cost']
+    cmc:str = dados['data'][0]['cmc']
+    p_r:str = (dados['data'][0]['power'] + '/' + dados['data'][0]['toughness'])
+    cor:str = dados['data'][0]['colors']
     refs = dados['data'][0]['related_uris']['gatherer']
     more = dados['has_more']
     if more == True:
